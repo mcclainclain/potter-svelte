c4c6d5f30128e79997713902d9943e60e6150f79
mcclainclain <matthew.mcclain08@icloud.com>
2023-01-16 14:18:58 -0600

    added an about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,55 @@
+<div class="card h-full m-10 p-20 text-center">
+	<h1 class="text-5xl font-bold">About this Page</h1>
+	<br /><br /><br />
+	<h3 class="font-bold">This page is still being updated.</h3>
+	<br /><br />
+	<hr class="!border-t-8" />
+	<br />
+	<h1 class="font-bold">Tools Used</h1>
+	<br /><br />
+	<ol class="list-disc text-left text-2xl font-bold">
+		<li>
+			<a href="https://hp-api.onrender.com/" target="_blank" rel="noreferrer">Harry Potter API</a>
+			<p>
+				The API that produces all characters, their attributes, and all the spells (to be added). To
+				learn more, visit the <a
+					href="https://github.com/KostaSav/hp-api"
+					target="_blank"
+					rel="noreferrer">GitHub</a
+				> page for this API.
+			</p>
+		</li>
+
+		<br />
+		<li>
+			<a href="https://kit.svelte.dev/" target="_blank" rel="noreferrer">SvelteKit</a>
+			<p>
+				A UI framework built on Svelte and Vite, allowing users to write neat components using their
+				folder structure, along with simple HTML, CSS, and JavaScript.
+			</p>
+		</li>
+		<br />
+		<li>
+			<a href="https://www.skeleton.dev" target="_blank" rel="noreferrer">skeleton.dev</a>
+			<p>
+				Controls most of the styles from this page. It is a package currently in beta that combines
+				the use of <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"
+					>TailwindCSS</a
+				>
+				and <a href="https://svelte.dev/" target="_blank" rel="noreferrer">Svelte</a>.
+			</p>
+		</li>
+	</ol>
+
+	<br /><br />
+	<hr class="!border-t-8" />
+	<br />
+	<br />
+	<h2 class="font-bold">
+		To learn more about what is coming next, visit the Github <a
+			href="https://github.com/mcclainclain/potter-svelte"
+			target="_blank"
+			rel="noreferrer">repository</a
+		> for this page.
+	</h2>
+</div>
