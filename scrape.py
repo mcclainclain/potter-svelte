f01eb14a83832917e4c9b1e9371d5b115799288f
mcclainclain <matthew.mcclain08@icloud.com>
2023-01-16 20:27:35 -0600

    removed unnecessary .py file

--- scrape.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-from bs4 import BeautifulSoup
-headers = {"Accept": "application/json"}
-response = requests.get(
-    'https://hp-api.onrender.com/api/characters', headers=headers)
-
-chars = response.json()
-print(chars[0])
